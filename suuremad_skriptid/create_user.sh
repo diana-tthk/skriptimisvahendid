25125e79eecfdcb246e0efaeecde449875ca3c44
diana-tthk <112874813+diana-tthk@users.noreply.github.com>
2022-11-11 12:33:44 +0000

    tund 11.11 ülesanne

--- suuremad_skriptid/create_user.sh
+++ suuremad_skriptid/create_user.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #Kasutaja loomine
 
+#Täiendada seda skripti
+#1. saab panna mitu kasutajanimet järjest 
+#2. kasutajanimed loetakse failist
+#3. Küsime kasutajat, kas ta tahab lugeda kasutajanimed failist (siis las paneb faili nimi) või sisestada käsitsi (üks või mitu)
+
 read -p "Username: " username
 #saab ka nii:
 #test=`grep -c "$username" /etc/passwd`
